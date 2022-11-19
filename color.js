f3f0c25752e746b63f168c33b5a8c461c9d3e3cc
Rafael Macedo Vitor <117114737+rarfel@users.noreply.github.com>
2022-11-19 20:46:50 -0300

    Forgot to remove console.log() :P

--- color.js
+++ color.js
@@ -4,7 +4,6 @@ function ColorMe(){
     let cores = ['red','blue','green','purple','yellow','white','orange']
     let randNumOne = (Math.floor(Math.random() * cores.length))
     let randNumTwo = (Math.floor(Math.random() * cores.length))
-    console.log(`numero1:${randNumOne}, numero2:${randNumTwo}`);
     root.style.setProperty('--colorOne',cores[randNumOne])
     root.style.setProperty('--colorTwo',cores[randNumTwo])
 }
